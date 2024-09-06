14c4266a2830b02efea367c22668562906fe6bc8
Jackson Wozniak <jjw30@comcast.net>
2024-09-06 11:31:50 -0400

    initial file commit

---
+++ backend/src/main/java/internal/api/application/FinanceDashboardApplication.java
@@ -0,0 +1,13 @@
+package internal.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceDashboardApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/internal/api/application/FinanceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package internal.api.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
